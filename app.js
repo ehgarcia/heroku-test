4362449704e409d331b9c426f55968d02ecc8d0b
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-08-29 21:30:33 -0300

    se cambia resolve por join en el app.get

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = ('path')
 const app = express()
 
 app.get('/', function(req,res){
-    res.sendFile(path.resolve(__dirname, './index.html'))
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.listen(process.env.PORT || 3000)
