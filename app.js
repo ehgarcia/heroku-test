eb6342945f2ecf91e9e8700b828ec6233c5e32d6
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-08-29 21:33:34 -0300

    se corrigio el servidor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require ('express')
-const path = ('path')
-const app = express()
+const express = require ('express');
+const path = require('path')
+const app = express();
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, './index.html'))
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./index.html'))
 })
 
 app.listen(process.env.PORT || 3000)
